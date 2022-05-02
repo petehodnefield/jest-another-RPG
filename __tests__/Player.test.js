5244bcac22bd112f70ca1c837812f82cb1fbdb3f
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-05-02 11:06:56 -0500

    player methods done

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -65,3 +65,31 @@ test("correct amount of health being subtracted from player", () => {
 
     expect(player.health).toBe(0)
 })
+
+test("gets player's attack value", () => {
+    const player = new Player('Dave')
+    player.strength = 10
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5)
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15)
+})
+
+test("potion was added correctly", () => {
+    const player = new Player('Dave')
+    const oldCount = player.inventory.length
+
+    player.addPotion(new Potion())
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount)
+})
+
+test("uses a potion from inventory", () => {
+    const player = new Player('Dave')
+    player.inventory = [new Potion(), new Potion(), new Potion()]
+    const oldCount = player.inventory.length
+    
+    player.usePotion(1)
+
+    expect(player.inventory.length).toBeLessThan(oldCount)
+
+})
